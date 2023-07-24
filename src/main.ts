ebde83e362af10415b9b08c65963041bfc879172
olkovmark <olkovmark@gmail.com>
2023-07-24 19:34:45 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  console.log('Start');
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn'],
   });
